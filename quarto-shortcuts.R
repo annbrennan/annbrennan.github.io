e38750585fbe2a8586bf24a0b745f9f002992012
annbrennan <ab105@geneseo.edu>
2023-12-11 15:26:18 -0500

    updatingg my website

---
+++ quarto-shortcuts.R
@@ -0,0 +1,22 @@
+### Quarto shortcuts
+
+# Alt+Ctrl+I : to create a R chunk
+
+# Ctrl+Shift+Enter : to run the code in the R chunk
+
+# Ctrl + Shift + K: to knit/render the Quarto file
+
+# Ctrl + Shift + C: to (de-)comment out a line in the Quarto file
+
+# To render all the Quarto files in your local working directory, 
+# execute the following command in Terminal:
+# quarto render
+# You should use this command when you make changes in _quarto.yml.
+
+# On Terminal within RStudio, execute the following three git commands one 
+# by one in Terminal, which will stage, commit, and push all the files to 
+# your GitHub repository:
+# git add .
+# git commit -m “ANY_MASSAGE”
+# git push -u origin main
+
